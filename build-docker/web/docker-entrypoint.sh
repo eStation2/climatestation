9bab542c842177e49b98a1357f9950b8e524debf
SimonettiDario <simodarigm@gmail.com>
2020-12-08 22:22:37 +0100

    Fix to start tornado in docker. Refer to Impact v1.12b from repo http://bitbucket.org/jrcimpact/impact4.git  Call with http://localhost:8899/IMPACT

--- build-docker/web/docker-entrypoint.sh
+++ build-docker/web/docker-entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#echo "Starting container $(hostname)"
-sleep 2
-#export PYTHONPATH=$PYTHONPATH:'var/www/estation2'
-python -c 'import webpy_esapp_helpers; webpy_esapp_helpers.importJRCRefWorkspaces(version=1)'
-sleep 2
-python -c 'from database.dbInstall.install_update_db_in_postgres_container import install_update_db; install_update_db()'
-#/setup_pythonenvironment.sh
-sleep 3
+##echo "Starting container $(hostname)"
+#sleep 2
+##export PYTHONPATH=$PYTHONPATH:'var/www/estation2'
+#python -c 'import webpy_esapp_helpers; webpy_esapp_helpers.importJRCRefWorkspaces(version=1)'
+#sleep 2
+#python -c 'from database.dbInstall.install_update_db_in_postgres_container import install_update_db; install_update_db()'
+##/setup_pythonenvironment.sh
+#sleep 3
+python apps/impact/Gui/libs_python/my_server.py localhost 8899 9999 &
 apache2ctl -D FOREGROUND
+
+
