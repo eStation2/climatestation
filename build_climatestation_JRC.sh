013502b2dc392582560b7a3aadf361539005a9ac
tklooju <jurvtk@gmail.com>
2021-03-16 16:19:11 +0100

    Implementing host user settings in web docker container

--- build_climatestation_JRC.sh
+++ build_climatestation_JRC.sh
@@ -4,7 +4,7 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
 # Build and run the climate station containers: mapserver, potsgres and web
-docker-compose -f ./docker-compose.yml up -d --build
+docker-compose -f ./docker-compose-JRC.yml up -d --build
 
 # Run script that installs or update the CS DB
 docker exec -ti postgres sh -c "/install_update_db.sh"
