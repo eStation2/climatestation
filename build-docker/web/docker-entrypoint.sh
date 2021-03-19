6aa6c7e0c93759446d08505a934e87c4448e3a73
tklooju <jurvtk@gmail.com>
2021-03-19 12:39:05 +0100

    LF convert

--- build-docker/web/docker-entrypoint.sh
+++ build-docker/web/docker-entrypoint.sh
@@ -2,26 +2,6 @@
 
 # echo $(id -un)
 
-# Create mandatory directories and set their permission
-mkdir -p /data
-mkdir -p /data/processing
-mkdir -p /data/ingest
-mkdir -p /data/ingest.wrong
-mkdir -p /data/static_data
-mkdir -p /data/static_data/completeness_bars
-mkdir -p /data/static_data/db_dump
-mkdir -p /data/static_data/docs
-mkdir -p /data/static_data/get_lists
-mkdir -p /data/static_data/layers
-mkdir -p /data/static_data/log
-mkdir -p /data/static_data/logos
-mkdir -p /data/static_data/requests
-mkdir -p /data/static_data/settings
-mkdir -p /tmp/climatestation
-mkdir -p /tmp/climatestation/services
-
-# chmod 775 -R /data
-
 USER_SETTINGS=/data/static_data/settings/user_settings.ini
 if [ ! -f "$USER_SETTINGS" ]; then
     echo "$USER_SETTINGS does not exist. Copy from source code."
